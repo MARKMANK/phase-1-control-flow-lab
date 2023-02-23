9fd43b735b32a220a288f0dd7f1c8b455a707863
kevinmarkman <kevin.w.markman@gmail.com>
2023-02-22 18:59:02 -0600

    Completes functions

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feetTraveled){
+  if (feetTraveled <= 400)
+    return 'This one is on me!';
+  if (feetTraveled >= 400 && feetTraveled <= 2000)
+    return 'That will be twenty bucks.';
+  else if(feetTraveled > 2500)
+    return 'No can do.'
+  else (feetTraveled > 2000)
+    return 'I will gladly take your thirty bucks.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(whatCity){
+  return whatCity == 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
